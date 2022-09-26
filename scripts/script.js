694a1fd97a89b4034a7a6e0858c027c64308563f
Bryan-OF <112858653+Bryan-OF@users.noreply.github.com>
2022-09-26 12:35:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,30 @@
 // JavaScript Document
 console.log("hi");
+
+/***********************/
+/* nav menu open/dicht */
+/***********************/
+
+var menuButton = document.querySelector("button");
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	var menu = document.querySelector("nav");
+	menu.classList.toggle("open");
+}
+
+
+
+
+
+/***************************/
+/* EERSTE STREEPJES BUTTON */
+/***************************/
+
+/* hamburger menu met bewegend strepen link:https://codepen.io/shooft/pen/dymRMBZ */
+var eersteStreepjesButton = document.querySelector("header button");
+eersteStreepjesButton.addEventListener("click", eersteStreepjesButtonKlik);
+function eersteStreepjesButtonKlik() {
+  eersteStreepjesButton.classList.toggle("menuOpen");
+}
